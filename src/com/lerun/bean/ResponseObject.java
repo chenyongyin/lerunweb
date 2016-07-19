27e05833b7e504ac51246ef725c783279ffd2eac
chenyongyin <wschenyongyin@qq.com>
2016-07-19 16:33:09 +0800

    aa

--- src/com/lerun/bean/ResponseObject.java
+++ src/com/lerun/bean/ResponseObject.java
@@ -2,4 +2,34 @@ package com.lerun.bean;
 
 public class ResponseObject {
 
+	int stage;// 状态码
+	Object datas;// 数据
+
+	public ResponseObject(int stage, Object datas) {
+		super();
+		this.stage = stage;
+		this.datas = datas;
+	}
+
+	public ResponseObject() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getStage() {
+		return stage;
+	}
+
+	public void setStage(int stage) {
+		this.stage = stage;
+	}
+
+	public Object getDatas() {
+		return datas;
+	}
+
+	public void setDatas(Object datas) {
+		this.datas = datas;
+	}
+
 }
